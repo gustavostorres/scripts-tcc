8fdb654852df6fa03b4ca70777b05aae4137cfc7
Torres <gustavotorresxp@gmail.com>
2024-02-09 10:46:43 -0300

    Update find-linux-vfcs.py

--- find-linux-vfcs.py
+++ find-linux-vfcs.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-bigvul = pd.read_csv("D:/Área de Trabalho/MSR_data_cleaned.csv", low_memory=False)
+bigvul = pd.read_csv("file/path/MSR_data_cleaned.csv", low_memory=False)
 linux = bigvul[bigvul["project"] == 'linux']
 linux.to_excel("D:/Área de Trabalho/linux.xlsx", sheet_name="linux", index=False)
